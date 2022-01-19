60c79037178cca9dfaea86b78e5099f7573c4ac9
georgediepiriye <georgediepiriye4u@gmail.com>
2022-01-19 23:57:47 +0100

    worked on products list page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+  
 
 `
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home';
+import ProductsList from './pages/ProductsList';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home/>
+    <ProductsList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
